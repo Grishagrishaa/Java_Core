2e495398555961e2da202b89d43ba32936a44d8a
Grisha <grikha.bomzh78@gmail.com>
2021-12-10 23:32:58 +0300

    First save

---
+++ home_work_1/src/homework/Names1.java
@@ -0,0 +1,23 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Names1 {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        String dad = "Вася";
+        String mom = "Анастасия";
+        String attempt = s.nextLine();
+        //System.out.println(attempt.equalsIgnoreCase(dad) || attempt.equalsIgnoreCase(mom));
+        if (attempt.equalsIgnoreCase(dad) || attempt.equalsIgnoreCase(mom)){
+            if (attempt.equalsIgnoreCase(dad)){
+                System.out.println("Привет!, отец!");
+            }else{
+                System.out.println("Я тебя так долго ждал, Мама!");
+            }
+        }else {
+            System.out.println("День добрый, а вы кто такой?");
+        }
+    }
+}
+
